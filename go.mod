651ed55fa2874788ca695599f71887a618c20607
Ganser <ganser.uu@gmail.com>
2023-10-31 21:03:43 +0800

    knot first go

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/hootuu/knot
+
+go 1.20
+
+replace github.com/hootuu/tome => ../tome
+
+replace github.com/hootuu/utils => ../utils
+
+require (
+	github.com/hootuu/tome v1.0.2
+	github.com/hootuu/utils v1.0.2
+)
---
+++ knot.go
@@ -0,0 +1,10 @@
+package knot
+
+import (
+	"github.com/hootuu/tome/bk"
+	"github.com/hootuu/utils/errors"
+)
+
+func Append(chainID bk.Chain, data bk.Invariable, signature string) (bk.BID, *errors.Error) {
+	return bk.NilBID, nil
+}
